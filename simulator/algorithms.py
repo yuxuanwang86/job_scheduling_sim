460f9906a7db9ddbaadd142271198abf58d939a0
YuxuanWang <yuxuan.wang@etu.utc.com>
2022-01-24 04:24:28 +0100

    test EASY

--- simulator/algorithms.py
+++ simulator/algorithms.py
@@ -28,6 +28,10 @@ Job
 """
 
 
+
+
+
+
 def fcfs(jobs, cluster, clock):
     """First Come, First Served scheduler.
 
@@ -52,13 +56,13 @@ def fcfs(jobs, cluster, clock):
     there are not enough nodes available), we DO NOT schedule
     others jobs from the queue (we'll wait).
     """
-    nextjob = jobs[0]  # we will schedule the first job from the queue
+    next_job = jobs[0]  # we will schedule the first job from the queue
 
-    if cluster.available_nodes >= nextjob.nodes:
+    if cluster.available_nodes >= next_job.nodes:
         # if we have enough available nodes, we can run the job.
         # For that we will take the N first available nodes
         # (where N is the number of nodes requested by the job)
-        return (True, nextjob)
+        return (True, next_job)
     else:
         # we do NOT have enough nodes, so we will not schedule applications
         return (False, None)
@@ -86,7 +90,10 @@ def ff(jobs, cluster, clock):
     This scheduler will schedule the first job in the queue that
     fits within the available nodes.
     """
-    # TODO
+    for job in jobs:
+        if cluster.available_nodes >= job.nodes:
+            return (True, job)
+    return (False, None)
 
 
 def sjf(jobs, cluster, clock):
@@ -113,7 +120,11 @@ def sjf(jobs, cluster, clock):
     It only considers jobs that could be run on the available nodes.
     In the case of a tie, it choses the job with the smallest identifier.
     """
-    # TODO
+    jobs.sort(key=lambda x: (x.requested_run_time, x.jobID))
+    for job in jobs:
+        if cluster.available_nodes >= job.nodes:
+            return (True, job)
+    return (False, None)
 
 
 def fcfs_easy(jobs, cluster, clock):
@@ -144,4 +155,38 @@ def fcfs_easy(jobs, cluster, clock):
     #    on the list that could be scheduled without delaying the
     #    first job (i.e., its requested_run_time should be smaller
     #    than the predicted start of the first job minus the current clock
+    #    requested_run_time < predicted start of the first job - current
     #    or it falls in the extra nodes available).
+    job = jobs[0]
+    if job.nodes <= cluster.available_nodes:
+        return (True, job)
+    else:
+        running_jobs = sorted(cluster.running_jobs.items(), key=lambda x: x[1].expected_end)
+        extra_nodes = 0
+        predicted_start_first_job = 0
+        for job in running_jobs:
+            _, job = job
+            if cluster.available_nodes + job.nodes >= jobs[0].nodes:
+                extra_nodes = cluster.available_nodes + job.nodes - jobs[0].nodes
+                predicted_start_first_job = job.requested_run_time + clock
+                break
+        # if not predicted_start_first_job:
+        #     return (False, None)
+        for i in range(0, len(jobs)):
+            if i == 0:
+                continue
+            job = jobs[i]
+            if running_jobs[0][1].expected_end <= clock:
+                break
+            elif job.nodes <= cluster.available_nodes:
+                if job.requested_run_time <= predicted_start_first_job - clock:
+                    return (True, job)
+                else:
+                    continue
+            else:
+                continue
+        return (False, None)
+
+        
+            
+
